84d3ada7b6603cd55b603fd75b2186e07c67b014
xotoverse <xotoverse@gmail.com>
2024-06-28 22:45:21 +0200

    fix

--- modules/cloudwatch/main.tf
+++ modules/cloudwatch/main.tf
@@ -1,11 +1,11 @@
 ################# cloudwatch
 
 resource "aws_cloudwatch_log_group" "xotocross-log-group" {
-  name = "${var.xotocross-log-group-name}"
+  name = var.xotocross-log-group-name
 }
 
 resource "aws_cloudwatch_metric_alarm" "xotocross-cpu-cap-alarm" {
-	alarm_name          = "xotocross-${var.xotocross-service-name}-${var.environment}-cpu-cap-alarm"
+	alarm_name          = "${var.xotocross-service-name}-${var.environment}-cpu-cap-alarm"
 	comparison_operator = "GreaterThanThreshold"
 	evaluation_periods  = "2"
 	metric_name         = "CPUUtilization"
@@ -13,7 +13,7 @@ resource "aws_cloudwatch_metric_alarm" "xotocross-cpu-cap-alarm" {
 	period              = "300"
 	statistic           = "Average"
 	threshold           = "80"
-	alarm_description   = "This metric checks if CPU utilization is above 80%"
+	alarm_description   = "xotocross metric checks if cpu utilization is above 80%"
 	alarm_actions       = [aws_sns_topic.xotocross-cpu-cap-alarm.arn]
 	dimensions = {
 		ClusterName = var.xotocross-ecs-cluster-id
@@ -21,12 +21,13 @@ resource "aws_cloudwatch_metric_alarm" "xotocross-cpu-cap-alarm" {
 	}
 }
 
-resource "aws_sns_topic_subscription" "xotocross-cpu-cap-email" {
+resource "aws_sns_topic_subscription" "email_subscriptions" {
+  for_each  = toset(var.xotocross-email)
   topic_arn = aws_sns_topic.xotocross-cpu-cap-alarm.arn
   protocol  = "email"
-  endpoint  = "xotosphere@gmail.com"
+  endpoint  = each.value
 }
 
 resource "aws_sns_topic" "xotocross-cpu-cap-alarm" {
-	name = "xotocross-${var.xotocross-service-name}-${var.environment}-cpu-cap-alarm"
+	name = "${var.xotocross-service-name}-${var.environment}-cpu-cap-alarm"
 }
--- modules/cloudwatch/variables.tf
+++ modules/cloudwatch/variables.tf
@@ -2,3 +2,4 @@ variable "environment" { description = "xotocross environment" }
 variable "xotocross-service-name" { description = "xotocross service name" }
 variable "xotocross-ecs-cluster-id" { description = "xotocross cluster name" }
 variable "xotocross-log-group-name" { description = "xotocross log group name name" }
+variable "xotocross-email" { description = "xotocross members to receive emails" }
