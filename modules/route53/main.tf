b36ce1d260f70ed6c19f9868e49f0d48b2e33eff
xotoverse <xotoverse@gmail.com>
2024-06-28 19:02:55 +0200

    fix small issue

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -10,14 +10,22 @@ resource "aws_route53_record" "domain" {
   }
 }
 
+# resource "aws_route53_record" "subdomain" {
+#   zone_id = var.xotocross-zone-id
+#   name    = "${var.xotocross-subdomain-name}-${var.environment}-${var.xotocross-domain-name}"
+#   type    = "A"
+
+#   alias {
+#     name                   = var.xotocross-alb-dns-name
+#     zone_id                = var.xotocross-alb-zone-id
+#     evaluate_target_health = true
+#   }
+# }
+
 resource "aws_route53_record" "subdomain" {
   zone_id = var.xotocross-zone-id
-  name    = "${var.xotocross-subdomain-name}.${var.xotocross-domain-name}"
-  type    = "A"
-
-  alias {
-    name                   = var.xotocross-alb-dns-name
-    zone_id                = var.xotocross-alb-zone-id
-    evaluate_target_health = true
-  }
+  name    = "${var.environment}-${var.xotocross-subdomain-name}-${var.xotocross-domain-name}"
+  type    = "CNAME"
+  records = [var.xotocross-domain-name]
+  ttl     = "300"
 }
