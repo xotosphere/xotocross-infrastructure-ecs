f2151ca4dc2155f4ae37ed184a34fc672e8cd564
xotoverse <xotoverse@gmail.com>
2024-06-28 20:49:56 +0200

    fix

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -1,21 +1,4 @@
 
-# resource "aws_route53_record" "xotocross-ns-records" {
-#   zone_id = aws_route53_zone.xotocross-zone.zone_id
-#   name    = var.xotocross-domain-name
-#   type    = "NS"
-#   ttl     = "300"
-#   records = [
-#     "ns-773.awsdns-32.net.",
-#     "ns-1313.awsdns-36.org.",
-#     "ns-1540.awsdns-00.co.uk.",
-#     "ns-9.awsdns-01.com."
-#   ]
-
-#   # lifecycle {
-#   #   prevent_destroy = true
-#   # }
-# }
-
 resource "aws_route53_record" "domain" {
   zone_id = data.aws_route53_zone.selected.zone_id
   name    = var.xotocross-domain-name
@@ -27,8 +10,6 @@ resource "aws_route53_record" "domain" {
   }
 }
 
-
-# This record points your subdomain (environment) to the ALB
 resource "aws_route53_record" "subdomain" {
   zone_id = data.aws_route53_zone.selected.zone_id
   name    = "${var.environment}.${var.xotocross-subdomain-name}.${var.xotocross-domain-name}"
