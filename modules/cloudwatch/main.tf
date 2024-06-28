fec45161f2a2b31241fa9f5f9e68f689ba52de81
xotoverse <xotoverse@gmail.com>
2024-06-28 22:31:03 +0200

    small fix

--- modules/cloudwatch/main.tf
+++ modules/cloudwatch/main.tf
@@ -4,8 +4,8 @@ resource "aws_cloudwatch_log_group" "xotocross-log-group" {
   name = "${var.xotocross-log-group-name}"
 }
 
-resource "aws_cloudwatch_metric_alarm" "xotocross-cpu-utilization-high" {
-	alarm_name          = "xotocross-${var.xotocross-service-name}-${var.environment}-utilization-high"
+resource "aws_cloudwatch_metric_alarm" "xotocross-cpu-cap-alarm" {
+	alarm_name          = "xotocross-${var.xotocross-service-name}-${var.environment}-cpu-cap-alarm"
 	comparison_operator = "GreaterThanThreshold"
 	evaluation_periods  = "2"
 	metric_name         = "CPUUtilization"
@@ -14,19 +14,19 @@ resource "aws_cloudwatch_metric_alarm" "xotocross-cpu-utilization-high" {
 	statistic           = "Average"
 	threshold           = "80"
 	alarm_description   = "This metric checks if CPU utilization is above 80%"
-	alarm_actions       = [aws_sns_topic.xotocross-cpu-utilization-high.arn]
+	alarm_actions       = [aws_sns_topic.xotocross-cpu-cap-alarm.arn]
 	dimensions = {
 		ClusterName = var.xotocross-ecs-cluster-id
 		ServiceName = var.xotocross-service-name
 	}
 }
 
-resource "aws_sns_topic_subscription" "xotocross-cpu-utilization-high_email" {
-  topic_arn = aws_sns_topic.xotocross-cpu-utilization-high.arn
+resource "aws_sns_topic_subscription" "xotocross-cpu-cap-email" {
+  topic_arn = aws_sns_topic.xotocross-cpu-cap-alarm.arn
   protocol  = "email"
   endpoint  = "xotosphere@gmail.com"
 }
 
-resource "aws_sns_topic" "xotocross-cpu-utilization-high" {
-	name = "xotocross-cpu-utilization-high"
+resource "aws_sns_topic" "xotocross-cpu-cap-alarm" {
+	name = "xotocross-${var.xotocross-service-name}-${var.environment}-cpu-cap-alarm"
 }
