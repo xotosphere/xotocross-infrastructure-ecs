24f93e9eb6b5d5cbdd2855a2f130e7f8b0746957
xotoverse <xotoverse@gmail.com>
2024-06-28 20:11:52 +0200

    fix : all

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -1,31 +1,15 @@
 resource "aws_route53_record" "domain" {
-  zone_id = var.xotocross-zone-id
+  zone_id = data.aws_route53_zone.selected.zone_id
   name    = var.xotocross-domain-name
-  type    = "A"
-
-  alias {
-    name                   = var.xotocross-alb-dns-name
-    zone_id                = var.xotocross-alb-zone-id
-    evaluate_target_health = true
-  }
+  type    = "CNAME"
+  records = [var.xotocross-alb-dns-name]
+  ttl     = "300"
 }
 
-# resource "aws_route53_record" "subdomain" {
-#   zone_id = var.xotocross-zone-id
-#   name    = "${var.xotocross-subdomain-name}-${var.environment}-${var.xotocross-domain-name}"
-#   type    = "A"
-
-#   alias {
-#     name                   = var.xotocross-alb-dns-name
-#     zone_id                = var.xotocross-alb-zone-id
-#     evaluate_target_health = true
-#   }
-# }
-
 resource "aws_route53_record" "subdomain" {
-  zone_id = var.xotocross-zone-id
-  name    = "${var.environment}-${var.xotocross-subdomain-name}-${var.xotocross-domain-name}"
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = "${var.environment}.${var.xotocross-subdomain-name}.${var.xotocross-domain-name}"
   type    = "CNAME"
-  records = [var.xotocross-domain-name]
+  records = [var.xotocross-alb-dns-name]
   ttl     = "300"
 }
