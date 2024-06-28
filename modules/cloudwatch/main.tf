80247b89f9b4d534446326ae9193a26c4a91e30d
xotoverse <xotoverse@gmail.com>
2024-06-29 00:04:28 +0200

    small fix

--- modules/cloudwatch/main.tf
+++ modules/cloudwatch/main.tf
@@ -14,20 +14,20 @@ resource "aws_cloudwatch_metric_alarm" "xotocross-cpu-cap-alarm" {
 	statistic           = "Average"
 	threshold           = "80"
 	alarm_description   = "xotocross metric checks if cpu utilization is above 80%"
-	alarm_actions       = [aws_sns_topic.xotocross-cpu-cap-alarm.arn]
+	alarm_actions       = [aws_sns_topic.xotocross-cpu-cap-sns.arn]
 	dimensions = {
 		ClusterName = var.xotocross-cluster-name
 		ServiceName = var.xotocross-service-name
 	}
 }
 
-resource "aws_sns_topic_subscription" "email_subscriptions" {
+resource "aws_sns_topic_subscription" "xotocross-cpu-cap-sns" {
   for_each  = toset(var.xotocross-email)
-  topic_arn = aws_sns_topic.xotocross-cpu-cap-alarm.arn
+  topic_arn = aws_sns_topic.xotocross-cpu-cap-sns.arn
   protocol  = "email"
   endpoint  = each.value
 }
 
-resource "aws_sns_topic" "xotocross-cpu-cap-alarm" {
+resource "aws_sns_topic" "xotocross-cpu-cap-sns" {
 	name = "${var.xotocross-service-name}-${var.environment}-cpu-cap-alarm"
 }
