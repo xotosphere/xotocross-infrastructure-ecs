1f8094c9f736649b14cf917fd799771676749031
xotoverse <xotoverse@gmail.com>
2024-06-28 17:56:10 +0200

    fix : domain

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -1,6 +1,22 @@
-resource "aws_route53_record" "xotocross-record" {
-  zone_id = var.xotocross-zone-id
+resource "aws_route53_zone" "xotocross-zone" {
+  name = var.xotocross-domain-name
+}
+
+resource "aws_route53_record" "domain" {
+  zone_id = aws_route53_zone.xotocross-zone.zone_id
   name    = var.xotocross-domain-name
+  type    = "A"
+
+  alias {
+    name                   = var.xotocross-alb-dns-name
+    zone_id                = var.xotocross-alb-zone-id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "subdomain" {
+  zone_id = aws_route53_zone.xotocross-zone.zone_id
+  name    = "${var.xotocross-subdomain-name}.${var.xotocross-domain-name}"
   type    = "A"
 
   alias {
--- modules/route53/variables.tf
+++ modules/route53/variables.tf
@@ -3,3 +3,4 @@ variable "xotocross-domain-name" { description = "xotocross domain name" }
 variable "xotocross-zone-id" { description = "xotocross zone id" }
 variable "xotocross-alb-dns-name" { description = "xotocross dns name of the alb" }
 variable "xotocross-alb-zone-id" { description = "xotocross zone id of the alb" }
+variable "xotocross-subdomain-name" { description = "xotocross subdomain name" }
