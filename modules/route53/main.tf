5ec5a6d8dc5d614292decb5c73a745e23d0658bf
xotoverse <xotoverse@gmail.com>
2024-06-29 10:09:55 +0200

    ifx

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -1,7 +1,14 @@
+# for domain names i was thinking we do the following : 
+
+# so we dont need prod with real urls for now. we can do urls based : 
+
+# we can have service.xotosphere.com for each service as name 
+# and then for environment we can have dev, staging, prod as subdomain like the config below 
+
 
 resource "aws_route53_record" "domain" {
   zone_id = data.aws_route53_zone.selected.zone_id
-  name    = var.xotocross-domain-name
+  name    = "${var.xotocross-subdomain-name}.${var.xotocross-domain-name}"
   type    = "A"
   alias {
     name                   = var.xotocross-alb-dns-name
