54985975653b70842703123bd63ec547c290e4dd
Alberto <42894888+Nenino2@users.noreply.github.com>
2024-06-30 17:16:52 +0200

    Fix

--- modules/elb/outputs.tf
+++ modules/elb/outputs.tf
@@ -8,10 +8,10 @@ output "xotocross-target-group-arns" {
   value       = { for k in keys(aws_lb_target_group.xotocross-tg) : k => aws_lb_target_group.xotocross-tg[k].arn }
 }
 
-# output "xotocross-alb-name" {
-#   value       = aws_lb.xotocross-alb.dns_name
-#   description = "xotocross dns name of the alb"
-# }
+output "xotocross-alb-name" {
+  value       = aws_lb.xotocross-alb.dns_name
+  description = "xotocross dns name of the alb"
+}
 
 output "xotocross-alb-zone-id" {
   value       = aws_lb.xotocross-alb.zone_id
