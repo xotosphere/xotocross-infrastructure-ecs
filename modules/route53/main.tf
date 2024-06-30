f9bdda6d7ad348340f8b641877a418ac77a6a05c
Alberto <42894888+Nenino2@users.noreply.github.com>
2024-06-30 15:50:57 +0200

    Small fix

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -6,16 +6,16 @@
 # and then for environment we can have dev, staging, prod as subdomain like the config below 
 
 
-resource "aws_route53_record" "domain" {
-  zone_id = data.aws_route53_zone.selected.zone_id
-  name    = "${var.xotocross-subdomain-name}.${var.xotocross-domain-name}"
-  type    = "A"
-  alias {
-    name                   = var.xotocross-alb-name
-    zone_id                = var.xotocross-alb-zone-id
-    evaluate_target_health = true
-  }
-}
+# resource "aws_route53_record" "domain" {
+#   zone_id = data.aws_route53_zone.selected.zone_id
+#   name    = "${var.xotocross-subdomain-name}.${var.xotocross-domain-name}"
+#   type    = "A"
+#   alias {
+#     name                   = var.xotocross-alb-name
+#     zone_id                = var.xotocross-alb-zone-id
+#     evaluate_target_health = true
+#   }
+# }
 
 resource "aws_route53_record" "subdomain" {
   zone_id = data.aws_route53_zone.selected.zone_id
