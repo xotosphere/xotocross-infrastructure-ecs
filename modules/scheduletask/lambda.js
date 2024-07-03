2d904cf9fbd79004c593d552540d72772cda2898
xotoverse <xotoverse@gmail.com>
2024-07-03 15:32:14 +0200

    Automated commit

--- modules/scheduletask/lambda.js
+++ modules/scheduletask/lambda.js
@@ -24,11 +24,18 @@ exports.handler = async ({ serviceName, action, taskCount }) => {
 	action === 'stop' ? await stopTasks(serviceName) : await changeTaskCount(serviceName, taskCount);
 };
 
+/**
+ * @param {string} serviceName
+ * @param {number} taskCount
+ */
 const changeTaskCount = async (serviceName, taskCount) => {
 	await config.client.send(new UpdateServiceCommand({ service: serviceName, desiredCount: taskCount }));
 	console.info(`desired task count of ${serviceName} changed to ${taskCount}`);
 };
 
+/**
+ * @param {string} serviceName
+ */
 const stopTasks = async (serviceName) => {
 	const { taskArns } = await config.client.send(new ListTasksCommand({ serviceName }));
 	for (const taskArn of taskArns) await config.client.send(new StopTaskCommand({ task: taskArn }));
