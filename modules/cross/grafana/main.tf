5edb551f0f5b35659fe86cc466ddccc360307671
xotoverse <xotoverse@gmail.com>
2024-07-12 16:31:31 +0200

    fix

--- modules/cross/grafana/main.tf
+++ modules/cross/grafana/main.tf
@@ -16,4 +16,5 @@ resource "grafana_dashboard" "xotocross-service-dashboard" {
   config_json = templatefile("${path.module}/dashboard.json.tpl", {
     xotocross-service-name = var.xotocross-service-name
   })
+  overwrite = true
 }
