c5cc90fb0e64f6b56357a40aa6b88fbef138721e
xotoverse <xotoverse@gmail.com>
2024-07-12 16:22:52 +0200

    Automated commit

--- modules/cross/grafana/main.tf
+++ modules/cross/grafana/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    grafana = {
+      source = "grafana/grafana"
+      version = "3.4.0"
+    }
+  }
+}
+
+provider "grafana" {
+  url  = "grafana.monitor.${var.environment}.${var.xotocross-domain-name}"
+  auth = "admin:${var.xotocross-password}"
+}
 
 resource "grafana_dashboard" "xotocross-service-dashboard" {
   config_json = templatefile("${path.module}/dashboard.json.tpl", {
