63aa06ae977e30da89296848f72974ead7a36ea1
xotoverse <xotoverse@gmail.com>
2024-07-12 13:18:54 +0200

    Automated commit

--- modules/cross/fluentbit/locals.tf
+++ modules/cross/fluentbit/locals.tf
@@ -39,9 +39,9 @@ locals {
     })
   }))
 
-  xotocross-container-definition-global = concat(var.xotocross-container-definition, var.xotocross-enable-monitor ? [local.xotocross-container-definition-fluentbit] : [])
-  xotocross-healthcheck-paths-global    = concat(var.xotocross-healthcheck-paths, var.xotocross-enable-monitor ? ["/api/v1/health"] : [])
-  xotocross-listener-hosts-global       = concat(var.xotocross-listener-hosts, var.xotocross-enable-monitor ? ["fluentbit.${var.xotocross-service-name}.${var.environment}.${var.xotocross-domain-name}"] : [])
-  xotocross-container-ports-global      = concat(var.xotocross-container-ports, var.xotocross-enable-monitor ? [2020, 24224] : [])
-  xotocross-host-ports-global           = concat(var.xotocross-host-ports, var.xotocross-enable-monitor ? [2020, 24224] : [])
+  xotocross-container-definition = concat(var.xotocross-container-definition, var.xotocross-enable-monitor ? [local.xotocross-container-definition-fluentbit] : [])
+  xotocross-healthcheck-paths    = concat(var.xotocross-healthcheck-paths, var.xotocross-enable-monitor ? ["/api/v1/health"] : [])
+  xotocross-listener-hosts       = concat(var.xotocross-listener-hosts, var.xotocross-enable-monitor ? ["fluentbit.${var.xotocross-service-name}.${var.environment}.${var.xotocross-domain-name}"] : [])
+  xotocross-container-ports      = concat(var.xotocross-container-ports, var.xotocross-enable-monitor ? [2020, 24224] : [])
+  xotocross-host-ports           = concat(var.xotocross-host-ports, var.xotocross-enable-monitor ? [2020, 24224] : [])
 }
---
+++ modules/cross/fluentbit/outputs.tf
@@ -0,0 +1,19 @@
+output "xotocross-container-definition" {
+  value = local.xotocross-container-definition
+}
+
+output "xotocross-healthcheck-paths" {
+  value = local.xotocross-healthcheck-paths
+}
+
+output "xotocross-listener-hosts" {
+  value = local.xotocross-listener-hosts
+}
+
+output "xotocross-container-ports" {
+  value = local.xotocross-container-ports
+}
+
+output "xotocross-host-ports" {
+  value = local.xotocross-host-ports
+}
