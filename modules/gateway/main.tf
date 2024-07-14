52ff3db69c50de22232d0f4592d88befc30c5da8
xotoverse <xotoverse@gmail.com>
2024-07-14 13:23:22 +0200

    Automated commit

--- modules/gateway/main.tf
+++ modules/gateway/main.tf
@@ -21,7 +21,7 @@ resource "aws_api_gateway_method" "xotocross-proxy" {
 	authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "xotocross_integration" {
+resource "aws_api_gateway_integration" "xotocross-integration" {
 	rest_api_id = aws_api_gateway_rest_api.xotocross-api.id
 	resource_id = aws_api_gateway_resource.xotocross-resource.id
 	http_method = aws_api_gateway_method.xotocross-proxy.http_method
