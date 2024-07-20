dc35418bdc10ff58e8db115fb6ba9ca8017e4a39
xotoverse <xotoverse@gmail.com>
2024-07-20 12:39:37 +0200

    Automated commit

--- modules/cross/fluentbit/locals.tf
+++ modules/cross/fluentbit/locals.tf
@@ -1,9 +1,9 @@
 locals {
   xotocross-container-fluentbit = jsondecode(templatefile("${path.module}/aws/task-container.tpl", {
-    xotocross-container-name      = "xotocross-${var.xotocross-service-name}-fluentbit"
-    xotocross-container-image     = "ghcr.io/xotosphere/fluentbit:latest"
-    xotocross-container-cpu       = 0
-    xotocross-container-memory    = 256
+    xotocross-container-name = "xotocross-${var.xotocross-service-name}-fluentbit"
+    xotocross-container-image = "ghcr.io/xotosphere/fluentbit:latest"
+    xotocross-container-cpu = 0
+    xotocross-container-memory = 256
     xotocross-container-essential = false
     xotocross-container-portmap = jsonencode([{ containerPort = 24224, hostPort = 24224, protocol = "tcp" },{ containerPort = 2020, hostPort = 2020, protocol = "tcp" }])
     xotocross-container-environment = jsonencode([
@@ -16,25 +16,25 @@ locals {
       { name = "AWS_REGION", value = var.region },
       { name = "LOG_GROUP", value = "/aws/ecs/xotocross-${var.xotocross-service-name}-${var.environment}-log" },
     ])
-    xotocross-container-loggroup    = "/aws/ecs/xotocross-${var.xotocross-service-name}-${var.environment}-log"
-    xotocross-container-region      = var.region
-    xotocross-container-command     = jsonencode([])
-    xotocross-container-dependency  = jsonencode([])
-    xotocross-container-entrypoint  = jsonencode([])
+    xotocross-container-loggroup = "/aws/ecs/xotocross-${var.xotocross-service-name}-${var.environment}-log"
+    xotocross-container-region = var.region
+    xotocross-container-command = jsonencode([])
+    xotocross-container-dependency = jsonencode([])
+    xotocross-container-entrypoint = jsonencode([])
     xotocross-container-healthcheck = "null"
 
     xotocross-container-firelensconfiguration = jsonencode({
       type = "fluentbit",
       options = {
-        config-file-type        = "file",
-        config-file-value       = "/fluent-bit/etc/fluent-bit-filter.conf"
+        config-file-type = "file",
+        config-file-value = "/fluent-bit/etc/fluent-bit-filter.conf"
       }
     })
   }))
 
   xotocross-container-definition = concat(var.xotocross-container-definition, var.xotocross-enable-monitor ? [local.xotocross-container-fluentbit] : [])
-  xotocross-healthcheck-pathlist    = concat(var.xotocross-healthcheck-pathlist, var.xotocross-enable-monitor ? ["/api/v1/health"] : [])
-  xotocross-listener-hostlist       = concat(var.xotocross-listener-hostlist, var.xotocross-enable-monitor ? ["fluentbit.${var.xotocross-service-name}.${var.environment}.${var.xotocross-domain-name}"] : [])
-  xotocross-container-portlist      = concat(var.xotocross-container-portlist, var.xotocross-enable-monitor ? [2020, 24224] : [])
-  xotocross-host-portlist           = concat(var.xotocross-host-portlist, var.xotocross-enable-monitor ? [2020, 24224] : [])
+  xotocross-healthcheck-pathlist = concat(var.xotocross-healthcheck-pathlist, var.xotocross-enable-monitor ? ["/api/v1/health"] : [])
+  xotocross-listener-hostlist = concat(var.xotocross-listener-hostlist, var.xotocross-enable-monitor ? ["fluentbit.${var.xotocross-service-name}.${var.environment}.${var.xotocross-domain-name}"] : [])
+  xotocross-container-portlist = concat(var.xotocross-container-portlist, var.xotocross-enable-monitor ? [2020, 24224] : [])
+  xotocross-host-portlist = concat(var.xotocross-host-portlist, var.xotocross-enable-monitor ? [2020, 24224] : [])
 }
