5c5ffd3c144770f0c51ba943065d7122da1197a8
xotoverse <xotoverse@gmail.com>
2024-07-21 22:47:07 +0200

    Automated commit

--- modules/cloudwatch/main.tf
+++ modules/cloudwatch/main.tf
@@ -1,4 +1,4 @@
 resource "aws_cloudwatch_log_group" "xotocross-ecs-loggroup" {
 	name = var.xotocross-ecs-loggroup-name
-  retention_in_days = 14
+  retention_in_days = var.xotocross-loggroup-retention
 }
--- modules/cloudwatch/variables.tf
+++ modules/cloudwatch/variables.tf
@@ -1,2 +1,3 @@
 variable "environment" { description = "xotocross environment (e.g. dev, stage, prod, infra)" }
 variable "xotocross-ecs-loggroup-name" { description = "xotocross log group name" }
+variable "xotocross-loggroup-retention" { description = "xotocross retention days for the loggroup" }
