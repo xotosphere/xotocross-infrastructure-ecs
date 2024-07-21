c60bd0dc271f8576dbb63553460cee248b5fec1c
xotoverse <xotoverse@gmail.com>
2024-07-21 15:57:25 +0200

    Automated commit

--- modules/elb/data.tf
+++ modules/elb/data.tf
@@ -1,3 +1,4 @@
 data "external" "certificate" {
+  #   program = ["bash", "-c", "aws acm list-certificates --region ${var.region} | jq -r '{arn: (.CertificateSummaryList[] | select(.DomainName == \"${var.xotocross-domain-name}.com\") | .CertificateArn // \"\")}'"]
   program = ["bash", "-c", "aws acm list-certificates --region eu-west-3 | jq -r '{arn: (.CertificateSummaryList[] | select(.DomainName == \"xotosphere.com\") | .CertificateArn // \"\")}'"]
 }
