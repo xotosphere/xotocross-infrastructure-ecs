ff0a12e9a3c90bbe3251110c70e3b21fecfc4c08
xotoverse <xotoverse@gmail.com>
2024-07-21 16:43:41 +0200

    Automated commit

--- modules/elb/data.tf
+++ modules/elb/data.tf
@@ -1,5 +1,5 @@
 data "external" "certificate" {
-  program = ["bash", "-c", "aws acm list-certificates --region ${var.region} | jq -r '.CertificateSummaryList[] | select(.DomainName == \"xotosphere.com\" and .Status == \"ISSUED\") | .CertificateArn // \"\"' | xargs -I {} echo -n '{\"arn\": \"{}\"}'"]
+  program = ["bash", "-c", "aws acm list-certificates --region ${var.region} | jq -r '.CertificateSummaryList[] | select(.DomainName == \"xotosphere.com\" and .Status == \"ISSUED\") | .CertificateArn // \"\"' | xargs -I {} echo -n '{\"arn\": \"{}\"}' || echo -n '{\"arn\": \"\"}'"]
   # program = ["bash", "-c", "aws acm list-certificates --region ${var.region} | jq -r '{arn: (.CertificateSummaryList[] | select(.DomainName == \"xotosphere.com\") | .CertificateArn // \"\")}'"]
 }
 
