77323e183a9f00272198884ec67be9455c3fdaf0
xotoverse <xotoverse@gmail.com>
2024-07-21 15:55:46 +0200

    Automated commit

--- modules/elb/main.tf
+++ modules/elb/main.tf
@@ -1,26 +1,4 @@
 
-
-output "xotocross-loadbalaner-listener-arnlist" {
-  value = { for k in keys(aws_lb_listener.xotocross-http-listener) : k => aws_lb_listener.xotocross-http-listener[k].arn }
-  description = "xotocross arn list of the alb listeners"
-}
-
-output "xotocross-targetgroup-arnlist" {
-  value = { for k in keys(aws_lb_target_group.xotocross-targetgroup) : k => aws_lb_target_group.xotocross-targetgroup[k].arn }
-  description = "xotocross arn list of the target groups"
-}
-
-output "xotocross-loadbalaner-name" {
-  value = aws_lb.xotocross-loadbalaner.dns_name
-  description = "xotocross dns name of the alb"
-}
-
-output "xotocross-loadbalaner-zone-id" {
-  value = aws_lb.xotocross-loadbalaner.zone_id
-  description = "xotocross zone id of the alb"
-}
-
-
 # resource "aws_cognito_user_pool" "xotocross-cognito-pool" {
 #   name = "xotocross-${var.environment}-pool"
 
