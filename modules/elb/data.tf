5f8a03e31330fde32e738898ca593f95984d784b
xotoverse <xotoverse@gmail.com>
2024-07-23 00:41:10 +0200

    Automated commit

--- modules/elb/data.tf
+++ modules/elb/data.tf
@@ -1,6 +1,9 @@
-data "external" "certificate" {
-  program = ["bash", "-c", "arn=$(aws acm list-certificates --region ${var.region} | jq -r '.CertificateSummaryList[] | select(.DomainName == \"${var.xotocross-domain-name}.com\" and .Status == \"ISSUED\") | .CertificateArn // \"\"'); if [ -z \"$arn\" ]; then echo -n '{\"arn\": \"\"}'; else echo -n '{\"arn\": \"$arn\"}'; fi"]
-  # program = ["bash", "-c", "aws acm list-certificates --region ${var.region} | jq -r '{arn: (.CertificateSummaryList[] | select(.DomainName == \"${var.xotocross-domain-name}.com\") | .CertificateArn // \"\")}'"]
-}
+# data "external" "certificate" {
+#   program = ["bash", "-c", "arn=$(aws acm list-certificates --region ${var.region} | jq -r '.CertificateSummaryList[] | select(.DomainName == \"${var.xotocross-domain-name}.com\" and .Status == \"ISSUED\") | .CertificateArn // \"\"'); if [ -z \"$arn\" ]; then echo -n '{\"arn\": \"\"}'; else echo -n '{\"arn\": \"$arn\"}'; fi"]
+#   # program = ["bash", "-c", "aws acm list-certificates --region ${var.region} | jq -r '{arn: (.CertificateSummaryList[] | select(.DomainName == \"${var.xotocross-domain-name}.com\") | .CertificateArn // \"\")}'"]
+# }
 
+data "external" "certificate" {
+  program = ["bash", "-c", "arn=$(aws acm list-certificates --region ${var.region} | jq -r '.CertificateSummaryList[] | select(.DomainName == \"${var.xotocross-domain-name}.com\" and (.Status == \"ISSUED\" or .Status == \"PENDING_VALIDATION\")) | .CertificateArn // \"\"'); if [ -z \"$arn\" ]; then echo -n '{\"arn\": \"\"}'; else echo -n '{\"arn\": \"$arn\"}'; fi"]
+}
 
