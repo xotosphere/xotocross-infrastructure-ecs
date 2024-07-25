d6f848af6067df5e90b8780be68dcb0cd815cb17
xotoverse <xotoverse@gmail.com>
2024-07-25 17:19:05 +0200

    fix : uncomment

--- modules/elb/main.tf
+++ modules/elb/main.tf
@@ -43,8 +43,8 @@ variable "xtcross-listener-hostlist" { description = "xtcross list of hosts for 
 ####################### DATA
 
 data "external" "certificate" {
-  # program = ["bash", "-c", "arn=$(aws acm list-certificates --region eu-west-3 | jq -r '.CertificateSummaryList[] | select(.DomainName == \"xotosphere.com\" and .Status == \"ISSUED\") | .CertificateArn' | head -n 1); jq --arg arn $arn '{\"arn\": $arn}'"]
-  program = ["bash", "-c", "jq --arg arn \"\" '{\"arn\": $arn}'"]
+  program = ["bash", "-c", "arn=$(aws acm list-certificates --region eu-west-3 | jq -r '.CertificateSummaryList[] | select(.DomainName == \"xotosphere.com\" and .Status == \"ISSUED\") | .CertificateArn' | head -n 1); jq --arg arn $arn '{\"arn\": $arn}'"]
+  # program = ["bash", "-c", "jq --arg arn \"\" '{\"arn\": $arn}'"]
 }
 
 ####################### RESOURCE
