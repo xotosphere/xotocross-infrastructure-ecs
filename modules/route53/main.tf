142ea4e0259f9f1a8d3807e9551902a52942f735
xotoverse <xotoverse@gmail.com>
2024-07-26 22:53:52 +0200

    Automated commit

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -10,13 +10,12 @@ variable "environment" { description = "xtcross environment" }
 variable "xtcross-domain-name" { description = "xtcross domain name" }
 variable "xtcross-loadbalaner-name" { description = "xtcross dns name of the alb" }
 variable "xtcross-loadbalaner-zone-id" { description = "xtcross zone id of the alb" }
-variable "xtcross-subdomain-name" { description = "xtcross subdomain name" }
 
 ####################### RESOURCE
 
 resource "aws_route53_record" "xtcross-service-record" {
   zone_id = data.aws_route53_zone.xtcross-zone.zone_id
-  name    = var.environment == "production" ? "*.${var.xtcross-domain-name}.com" : "${var.xtcross-subdomain-name}.${var.environment}.${var.xtcross-domain-name}.com"
+  name    = var.environment == "production" ? "*.${var.xtcross-domain-name}.com" : "*.${var.environment}.${var.xtcross-domain-name}.com"
   type    = "CNAME"
   ttl     = "300"
   records = [var.xtcross-loadbalaner-name]
