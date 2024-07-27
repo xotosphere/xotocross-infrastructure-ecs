ea2fb2353db12b253b00af797a67fab01e241415
xotoverse <xotoverse@gmail.com>
2024-07-27 18:01:28 +0200

    Automated commit

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -4,32 +4,36 @@ data "aws_route53_zone" "xtcross-zone" {
   name = "${var.xtcross-domain-name}.com"
 }
 
-####################### VARIABLE
+data "aws_route53_record" "xtcross-service-record-production" {
+  count  = var.environment == "production" ? 1 : 0
+  zone_id = data.aws_route53_zone.xtcross-zone.zone_id
+  name    = "*.${var.xtcross-domain-name}.com"
+  type    = "CNAME"
+}
 
-variable "environment" { description = "xtcross environment" }
-variable "xtcross-domain-name" { description = "xtcross domain name" }
-variable "xtcross-loadbalaner-name" { description = "xtcross dns name of the alb" }
-variable "xtcross-loadbalaner-zone-id" { description = "xtcross zone id of the alb" }
+data "aws_route53_record" "xtcross-service-record-wildcard" {
+  count  = var.environment != "production" ? 1 : 0
+  zone_id = data.aws_route53_zone.xtcross-zone.zone_id
+  name    = "*.${var.environment}.${var.xtcross-domain-name}.com"
+  type    = "CNAME"
+}
 
 ####################### RESOURCE
 
-resource "aws_route53_record" "xtcross-service-record" {
+resource "aws_route53_record" "xtcross-service-record-production" {
+  count  = var.environment == "production" && element(data.aws_route53_record.xtcross-service-record-production.*.records, 0) != var.xtcross-loadbalaner-name ? 1 : 0
   zone_id = data.aws_route53_zone.xtcross-zone.zone_id
-  name    = var.environment == "production" ? "*.${var.xtcross-domain-name}.com" : "*.${var.environment}.${var.xtcross-domain-name}.com"
+  name    = "*.${var.xtcross-domain-name}.com"
   type    = "CNAME"
   ttl     = "300"
   records = [var.xtcross-loadbalaner-name]
-
-  lifecycle {
-    create_before_destroy = true
-    ignore_changes = [records]
-  }
 }
 
-# resource "aws_route53_record" "xtcross-cognito-record" {
-#   zone_id = data.aws_route53_zone.xtcross-zone.zone_id
-#   name    = "authorizer.${var.xtcross-domain-name}.com"
-#   type    = "CNAME"
-#   ttl     = "300"
-#   records = [var.xtcross-loadbalaner-name]
-# }
+resource "aws_route53_record" "xtcross-service-record-wildcard" {
+  count  = var.environment == "production" && element(data.aws_route53_record.xtcross-service-record-wildcard.*.records, 0) != var.xtcross-loadbalaner-name ? 1 : 0
+  zone_id = data.aws_route53_zone.xtcross-zone.zone_id
+  name    = "*.${var.environment}.${var.xtcross-domain-name}.com"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [var.xtcross-loadbalaner-name]
+}
