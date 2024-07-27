20b47281567944078db08ad290591bc81b0dddea
xotoverse <xotoverse@gmail.com>
2024-07-27 18:15:25 +0200

    fix

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -5,6 +5,7 @@ variable "xtcross-domain-name" { description = "xtcross domain name" }
 variable "xtcross-loadbalaner-name" { description = "xtcross dns name of the alb" }
 variable "xtcross-loadbalaner-zone-id" { description = "xtcross zone id of the alb" }
 
+
 ####################### DATA
 
 data "aws_route53_zone" "xtcross-zone" {
@@ -28,7 +29,7 @@ data "aws_route53_record" "xtcross-service-record-wildcard" {
 ####################### RESOURCE
 
 resource "aws_route53_record" "xtcross-service-record-production" {
-  count  = var.environment == "production" && element(data.aws_route53_record.xtcross-service-record-production.*.records, 0) != var.xtcross-loadbalaner-name ? 1 : 0
+  count  = var.environment == "production" && length(try(data.aws_route53_record.xtcross-service-record-production[0].records, [])) == 0 ? 1 : 0
   zone_id = data.aws_route53_zone.xtcross-zone.zone_id
   name    = "*.${var.xtcross-domain-name}.com"
   type    = "CNAME"
@@ -37,7 +38,7 @@ resource "aws_route53_record" "xtcross-service-record-production" {
 }
 
 resource "aws_route53_record" "xtcross-service-record-wildcard" {
-  count  = var.environment == "production" && element(data.aws_route53_record.xtcross-service-record-wildcard.*.records, 0) != var.xtcross-loadbalaner-name ? 1 : 0
+  count  = var.environment != "production" && length(try(data.aws_route53_record.xtcross-service-record-wildcard[0].records, [])) == 0 ? 1 : 0
   zone_id = data.aws_route53_zone.xtcross-zone.zone_id
   name    = "*.${var.environment}.${var.xtcross-domain-name}.com"
   type    = "CNAME"
