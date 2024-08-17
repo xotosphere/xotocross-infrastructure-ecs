d499db39860f41d19e4c46141464314f3ae30ce4
xotoverse <xotoverse@gmail.com>
2024-08-17 19:48:10 +0200

    auto

--- modules/cross/grafana/main.tf
+++ modules/cross/grafana/main.tf
@@ -22,30 +22,33 @@ variable "xtcross-domain-name" { description = "xtcross domain name" }
 variable "xtcross-password" { description = "xtcross password" }
 variable "xtcross-username" { description = "xtcross username" }
 variable "xtcross-container-namelist" { description = "xtcross list of container names" }
+variable "xtcross-enable-monitor" { description = "xtcross enable monitor" }
 
 ####################### RESOURCE
 
 locals {
-  dashboard_container_panel_list = [
+  dashboard_container_panel_list = var.xtcross-enable-monitor ? [
     for container_name in var.xtcross-container-namelist : jsondecode(templatefile("${path.module}/dashboard-container-panel.json.tpl", {
       cost_project   = var.xtcross-service-name,
       container_name = container_name
     }))
-  ]
+  ] : []
 
-  dashboard_json = templatefile("${path.module}/dashboard.json.tpl", {
+  dashboard_json = var.xtcross-enable-monitor ? templatefile("${path.module}/dashboard.json.tpl", {
     cost_project                   = var.xtcross-service-name,
     environment                    = var.environment,
     dashboard_container_panel_list = join(",", [for item in local.dashboard_container_panel_list : jsonencode(item)])
-  })
+  }) : ""
 }
 
 resource "local_file" "dashboard_snapshot" {
-  content  = local.dashboard_json
-  filename = "${path.module}/dashboard_snapshot.json"
+  count     = var.xtcross-enable-monitor ? 1 : 0
+  content   = local.dashboard_json
+  filename  = "${path.module}/dashboard_snapshot.json"
 }
 
 resource "grafana_dashboard" "xtcross-service-dashboard" {
-  config_json = local.dashboard_json
-  overwrite   = true
+  count        = var.xtcross-enable-monitor ? 1 : 0
+  config_json  = local.dashboard_json
+  overwrite    = true
 }
