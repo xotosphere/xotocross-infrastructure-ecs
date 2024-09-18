4cd9a61ab5eddf29e0a0bbae4a69e91162f4e944
Alberto <halbtale@gmail.com>
2024-09-18 15:30:44 +0200

    Fix

--- modules/cross/fluentbit/locals.tf
+++ modules/cross/fluentbit/locals.tf
@@ -2,13 +2,13 @@
 
 locals {
   xtcross-container-fluentbit = jsondecode(templatefile("${path.module}/aws/task-container.tpl", {
-    xtcross-container-name = "${var.prefix}-${var.xtcross-service-name}-fluentbit"
-    xtcross-container-image = "ghcr.io/xotosphere/fluentbit:latest"
+    xtcross-container-name      = "${var.prefix}-${var.xtcross-service-name}-fluentbit"
+    xtcross-container-image     = "ghcr.io/xotosphere/fluentbit:latest"
     xtcross-container-essential = false
-    xtcross-container-portmap = jsonencode(concat(var.xtcross-enable-prometheus ? [{ containerPort = 2020, hostPort = 2020, protocol = "tcp", name = "port-${2020}" }] : [], []))
+    xtcross-container-portmap   = jsonencode(concat(var.xtcross-enable-prometheus ? [{ containerPort = 2020, hostPort = 2020, protocol = "tcp", name = "port-${2020}" }] : [], []))
     xtcross-container-environment = jsonencode([
       { name = "ENVIRONMENT", value = var.environment },
-      { name = "LOKI_HOST", value = "${var.prefix}-monitor-loki.${var.environment}.local", },
+      { name = "LOKI_HOST", value = "xtcross-monitor-loki.${var.environment}.local", },
       { name = "LOKI_PORT", value = "3100" },
       { name = "HTTPS", value = "Off" },
       { name = "HTTPS_VERIFY", value = "Off" },
@@ -18,17 +18,17 @@ locals {
       { name = "AWS_REGION", value = var.region },
       { name = "LOG_GROUP", value = "/aws/ecs/${var.prefix}-${var.xtcross-service-name}-${var.environment}" },
     ])
-    xtcross-container-command = jsonencode([])
-    xtcross-container-dependency = jsonencode([])
-    xtcross-container-entrypoint = jsonencode([])
-    xtcross-container-healthcheck = "null"
-    xtcross-logconfiguration = jsonencode({ logDriver = "awslogs", options = { "awslogs-group" = "/aws/ecs/${var.prefix}-${var.xtcross-service-name}-${var.environment}", "awslogs-region" = var.region } })
+    xtcross-container-command               = jsonencode([])
+    xtcross-container-dependency            = jsonencode([])
+    xtcross-container-entrypoint            = jsonencode([])
+    xtcross-container-healthcheck           = "null"
+    xtcross-logconfiguration                = jsonencode({ logDriver = "awslogs", options = { "awslogs-group" = "/aws/ecs/${var.prefix}-${var.xtcross-service-name}-${var.environment}", "awslogs-region" = var.region } })
     xtcross-container-firelensconfiguration = jsonencode({ type = "fluentbit", options = { config-file-type = "file", config-file-value = "/fluent-bit/etc/fluent-bit-filter.conf" } })
   }))
 
   xtcross-container-definition = concat(var.xtcross-container-definition, var.xtcross-enable-monitor ? [local.xtcross-container-fluentbit] : [])
   xtcross-healthcheck-pathlist = var.xtcross-healthcheck-pathlist
-  xtcross-listener-hostlist = var.xtcross-listener-hostlist
-  xtcross-container-portlist = var.xtcross-container-portlist
-  xtcross-host-portlist = var.xtcross-host-portlist
+  xtcross-listener-hostlist    = var.xtcross-listener-hostlist
+  xtcross-container-portlist   = var.xtcross-container-portlist
+  xtcross-host-portlist        = var.xtcross-host-portlist
 }
