ded8a777751033e13e92699c57232d870e2f6914
Alberto <halbtale@gmail.com>
2025-01-28 20:20:46 +0100

    fix: bug

--- modules/elb/main.tf
+++ modules/elb/main.tf
@@ -61,8 +61,13 @@ locals {
   certificate   = local.hasCert ? local.prod_cert_arn : null
 }
 
+data "aws_lb_listener" "xtcross-https-redirection" {
+  load_balancer_arn = data.aws_lb.xtcross-loadbalancer.arn
+  port              = 80
+}
+
 resource "aws_lb_listener" "xtcross-https-redirection" {
-  count             = local.hasCert ? 1 : 0
+  count             = (local.hasCert && length(data.aws_lb_listener.xtcross-https-redirection) == 0) ? 1 : 0
   load_balancer_arn = data.aws_lb.xtcross-loadbalancer.arn
   port              = 80
   protocol          = "HTTP"
@@ -78,7 +83,13 @@ resource "aws_lb_listener" "xtcross-https-redirection" {
   }
 }
 
+data "aws_lb_listener" "xtcross-http-listener" {
+  load_balancer_arn = data.aws_lb.xtcross-loadbalancer.arn
+  port              = 443
+}
+
 resource "aws_lb_listener" "xtcross-http-listener" {
+  count             = length(data.aws_lb_listener.xtcross-http-listener) == 0 ? 1 : 0
   load_balancer_arn = data.aws_lb.xtcross-loadbalancer.arn
   port              = local.hasCert ? 443 : 80
   certificate_arn   = local.certificate
@@ -96,9 +107,10 @@ resource "aws_lb_listener" "xtcross-http-listener" {
   }
 }
 
+
 resource "aws_lb_listener_rule" "xtcross-http-listener-rule" {
   for_each     = toset([for idx in range(0, length(var.xtcross-listener-hostlist)) : tostring(idx)])
-  listener_arn = aws_lb_listener.xtcross-http-listener.arn
+  listener_arn = length(data.aws_lb_listener.xtcross-http-listener) > 0 ? data.aws_lb_listener.xtcross-http-listener[0].arn : aws_lb_listener.xtcross-http-listener[0].arn
 
   action {
     type             = "forward"
