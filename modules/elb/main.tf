05e96f6bf0aa0a2344b185d79a8c7e7f07613cda
Alberto <halbtale@gmail.com>
2025-01-29 20:28:05 +0100

    fix

--- modules/elb/main.tf
+++ modules/elb/main.tf
@@ -56,18 +56,15 @@ resource "local_file" "certificate_snapshot" {
 ######################
 
 locals {
-  prod_cert_arn = data.external.xtcross-certificate.result["arn"]
-  hasCert       = local.prod_cert_arn != ""
-  certificate   = local.hasCert ? local.prod_cert_arn : null
-}
-
-data "aws_lb_listener" "xtcross-https-redirection" {
-  load_balancer_arn = data.aws_lb.xtcross-loadbalancer.arn
-  port              = 80
+  prod_cert_arn              = data.external.xtcross-certificate.result["arn"]
+  hasCert                    = local.prod_cert_arn != ""
+  certificate                = local.hasCert ? local.prod_cert_arn : null
+  https_redirection_listener = try(data.aws_lb_listener.xtcross-https-redirection, null)
+  http_listener              = try(data.aws_lb_listener.xtcross-http-listener, null)
 }
 
 resource "aws_lb_listener" "xtcross-https-redirection" {
-  count             = (local.hasCert && data.aws_lb_listener.xtcross-https-redirection == null) ? 1 : 0
+  count             = (local.hasCert && local.https_redirection_listener == null) ? 1 : 0
   load_balancer_arn = data.aws_lb.xtcross-loadbalancer.arn
   port              = 80
   protocol          = "HTTP"
@@ -83,13 +80,8 @@ resource "aws_lb_listener" "xtcross-https-redirection" {
   }
 }
 
-data "aws_lb_listener" "xtcross-http-listener" {
-  load_balancer_arn = data.aws_lb.xtcross-loadbalancer.arn
-  port              = 443
-}
-
 resource "aws_lb_listener" "xtcross-http-listener" {
-  count             = data.aws_lb_listener.xtcross-http-listener == null ? 1 : 0
+  count             = local.http_listener == null ? 1 : 0
   load_balancer_arn = data.aws_lb.xtcross-loadbalancer.arn
   port              = local.hasCert ? 443 : 80
   certificate_arn   = local.certificate
@@ -107,10 +99,9 @@ resource "aws_lb_listener" "xtcross-http-listener" {
   }
 }
 
-
 resource "aws_lb_listener_rule" "xtcross-http-listener-rule" {
   for_each     = toset([for idx in range(0, length(var.xtcross-listener-hostlist)) : tostring(idx)])
-  listener_arn = data.aws_lb_listener.xtcross-http-listener != null ? data.aws_lb_listener.xtcross-http-listener.arn : aws_lb_listener.xtcross-http-listener[0].arn
+  listener_arn = local.http_listener != null ? local.http_listener.arn : aws_lb_listener.xtcross-http-listener[0].arn
 
   action {
     type             = "forward"
